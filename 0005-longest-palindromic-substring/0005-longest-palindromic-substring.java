1225bc89db5f59578f4cb40af5e36f40d55eedd7
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-05-16 21:41:56 +0530

    Time: 6 ms (99.22%), Space: 42.7 MB (60.41%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,25 +1,27 @@
 class Solution {
-    public static int max = 0;
-    public static int l = 0;
-    public static void palin(String s,int start,int end){
-        while(start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
-            start--;
-            end++;
-        }
-        if(max < end -start- 1) {
-            max = end-start- 1;
-            l = start+1;
-        }
-    }
+    int maxLen = 0;
+    int lo = 0;
     public String longestPalindrome(String s) {
-        int n=s.length();
+        char[] input = s.toCharArray();
         if(s.length() < 2) {
             return s;
         }
-        for(int i=0;i<n;i++){
-                palin(s,i,i);
-                palin(s,i,i+1);
+        
+        for(int i = 0; i<input.length; i++) {
+            expandPalindrome(input, i, i);
+            expandPalindrome(input, i, i+1);
         }
-        return s.substring(l,l+max);
+        return s.substring(lo, lo+maxLen);
+    }
+    
+    public void expandPalindrome(char[] s, int j, int k) {
+        while(j >= 0 && k < s.length && s[j] == s[k]) {
+            j--;
+            k++;
+        }
+        if(maxLen < k - j - 1) {
+            maxLen = k - j - 1;
+            lo = j+1;
+        }
     }
 }
