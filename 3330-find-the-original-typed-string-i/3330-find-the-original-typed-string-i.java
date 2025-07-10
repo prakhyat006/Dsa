fb83f84e7ea2eb8eb099b2b65d0bae9599055f8c
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-07-10 10:13:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -1,15 +1,5 @@
 class Solution {
-    public int possibleStringCount(String word) {
-       int n=word.length();
-       int count=0;
-       Character a=word.charAt(0);
-       for(int i=0;i<n;i++){
-        if(word.charAt(i)==a){
-            count++;
-        }else{
-            a=word.charAt(i);
-        }
-       }
-       return count;
+    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
+        
     }
 }
