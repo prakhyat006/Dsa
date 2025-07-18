d13f598393ec5271f094e6b56b06c998f6c25aad
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-07-18 10:16:53 +0530

    Time: 1 ms (99.48%), Space: 46.7 MB (68.53%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -3,8 +3,9 @@ class Solution {
         int num[] =new int[n+1];
         num[0]=0;
         if(n==0)return num;
-        
-        num[1]=1;num[2]=1;
+        num[1]=1;
+        if(n==1)return num;
+        num[2]=1;
         for(int i=3;i<=n;i++){
             num[i]=num[i/2]+i%2;
         }
