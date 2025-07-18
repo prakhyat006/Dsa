c7b50e7aa830221633023ccfe2e117853a65ff93
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-07-18 10:13:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int num[] =new int[n+1];
+        num[0]=0;
+        if(n==0)return num;
+        
+        num[1]=1;num[2]=1;
+        for(int i=3;i<=n;i++){
+            num[i]=num[i/2]+i%2;
+        }
+        return num;
+    }
+}
