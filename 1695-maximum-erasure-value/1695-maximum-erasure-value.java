c964ebf6dda0ec9e327d35f9d2a83c618953f371
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-07-22 09:39:38 +0530

    Time: 132 ms (5.3%), Space: 60.4 MB (50.16%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.java
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -3,22 +3,24 @@ import java.util.*;
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         ArrayList<Integer> ls = new ArrayList<>();
-        int start = 0, maxSize = 0, maxSum = 0, currSum = 0;
-        for (int i = 0; i < nums.length;i++){
-            while (ls.contains(nums[i])) {
+        Set<Integer> set = new HashSet<>();
+
+        int start = 0, maxSum = 0, currSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (set.contains(nums[i])) {
+                set.remove(ls.get(0));
                 currSum -= ls.get(0);
                 ls.remove(0);
                 start++;
             }
             ls.add(nums[i]);
+            set.add(nums[i]);
             currSum += nums[i];
-            if (maxSize < ls.size()) {
-                maxSize = ls.size();
-            }
             maxSum = Math.max(maxSum, currSum);
         }
 
-        System.out.println(start + " " + maxSize);
+        System.out.println(start + " " + ls.size());
         return maxSum;
     }
 }
