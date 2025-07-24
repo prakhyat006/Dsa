a62ac577ae58e4ad14dc2ded264e61174b46b00d
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-07-24 12:35:56 +0530

    Time: 1 ms (99.47%), Space: 44.8 MB (51.23%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,14 +1,27 @@
 class Solution {
+    private int soldiers(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == 1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
     public int[] kWeakestRows(int[][] mat, int k) {
         int n=mat.length;
         int m=mat[0].length;
         int[] arr=new int[n];
         for(int i=0;i<n;i++){
-            int c=0;
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==1)c++;
-            }
-            arr[i]=c;
+            int c=0;      
+            arr[i]=soldiers(mat[i]);
         }
         PriorityQueue<Integer> ls = new PriorityQueue<>((i, j) -> {
         if(arr[i]==arr[j]) return  i-j;
