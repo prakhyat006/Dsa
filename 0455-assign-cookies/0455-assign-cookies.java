ae6e0aabac957b519e09fe47081b61d14c39aa6f
prakhyat006 <152693476+prakhyat006@users.noreply.github.com>
2025-08-26 09:11:24 +0530

    Time: 10 ms (79.02%), Space: 48.7 MB (45.76%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int n=g.length;
+        int m=s.length;
+        int i=0;
+        int j=0;
+        while(i<n && j<m){
+            if(g[i]<=s[j]){
+                i++;
+                j++;
+            }else j++;
+        }
+        return i;
+    }
+}
